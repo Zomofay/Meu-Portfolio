ff7bd6906315bf5ae958bed44da1aa72dc6b1bed
ツ Anderson Filho <126679604+Zomofay@users.noreply.github.com>
2023-11-06 08:56:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-const sidebar = document.querySelector('.sidebar');
-const sidebarToggle = document.querySelector('.sidebar-toggle');
-
 sidebarToggle.addEventListener('click', () => {
   sidebar.classList.toggle('active');
 });
+
+const sidebar = document.querySelector('.sidebar');
+const sidebarToggle = document.querySelector('.sidebar-toggle');
+
+// Selecione os botões de visualização de projeto
+const projectButtons = document.querySelectorAll('.project-button');
+
+projectButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const projectId = button.getAttribute('data-project-id');
+    const project = document.getElementById(projectId);
+    
+    if (project) {
+      // Remova a classe "active" de todos os projetos
+      const allProjects = document.querySelectorAll('.project');
+      allProjects.forEach(proj => {
+        proj.classList.remove('active');
+      });
+      
+      // Adicione a classe "active" ao projeto selecionado
+      project.classList.add('active');
+      sidebar.classList.add('active');
+    }
+  });
+});
+
+// Adicione um evento de clique ao botão "Fechar" na barra lateral para ocultá-la
+const closeButton = document.querySelector('.sidebar-close-button');
+if (closeButton) {
+  closeButton.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+  });
+};
